11e797ed8ff855f63690549e3ec958b6d68b06b8
Seamus G <seamus.g91@gmail.com>
2019-08-31 20:27:26 +0100

    add debug param

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from febiweb import app
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Input params')
+    parser.add_argument('-d', '--debug', action='store_true', help='Run in debug mode')
+    args = parser.parse_args()
+
+    app.run(host='0.0.0.0', port=80, debug=args.debug)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
