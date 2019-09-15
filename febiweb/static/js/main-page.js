b71339e22f8d4995011fff8fd30fcf0d2f394346
Seamus G <seamus.g91@gmail.com>
2019-09-15 11:35:46 +0100

    Tidying

--- febiweb/static/js/main-page.js
+++ febiweb/static/js/main-page.js
@@ -1,75 +1,71 @@
 
-        // const rightArrows = document.getElementsByClassName("rightarrow")
 
         folderSizes = ['120', '240', '360', '480', '600', '720', '840', '960']
-
         
-        // mainImgs[0].src = "{{ url_for('static', filename='assets/product1/1-1.jpg') }}"
+        const mainImgs = document.getElementsByClassName("product-main-img")
+        
         pageWidth = getWidth()
-        mainImgs[0].src = whichImageForSize(mainImPath1, pageWidth)
-        mainImgs[1].src = whichImageForSize(mainImPath2, pageWidth)
-        console.log("Main im is set to: ", mainImgs[0].src)
-        console.log("Main im 2 is set to: ", mainImgs[1].src)
+        for (let i=0; i<mainImgs.length; i++){
+            mainImgs[i].src = PRODUCT_IMAGES[i][0]
+        }
 
-        // let startIndex = [0, 0, 0]
-        let startIndex = [0, 0]
+        // For each main image set click listeners on the left and right of the image
+        // Load up the thumbnails of each
         for (var i=0; i<mainImgs.length; i++){
-
-            // Set the thumbnails
-            setImages(startIndex[i], i, mainImgs[i])
-            
-            // The img tag for the main image has a click listener. 
-            // This is independent of what img src is in the img tag
+            // This click listener is independent of what img src is in the img tag
             mainImgs[i].addEventListener('click', clickPrevNext)
             // Store the index so we know what image is next/previous
             mainImgs[i].imageIndx = 0
             // Which product number is this
             mainImgs[i].prodIndx = i
 
+            // Set the thumbnails
+            setImages(i, mainImgs[i])
         }
-        console.log("main width: ", mainImgs[0].width)
-        // for (var i=0; i<rightArrows.length; i++){
-        //     rightArrows[i].addEventListener('click', rotateImages)
-        //     rightArrows[i].imageIndex = i
-        // }
 
-        function setImages(start, productIndex, mainim) {
-            // maxImages = 6   // What is this 
-            febImages = allims[productIndex]
-            
-            // imgRow = elemByIndex(productIndex, "choiceblock")
+        // Set the thumbnails on a main image
+        function setImages(productIndex, mainim) {
+            // Images for this specific product
+            febImages = PRODUCT_IMAGES[productIndex]
+            // Find the scroll bar for this product
             imgRow = elemByIndex(productIndex, "scroll-thumbnail")
             let febiImg;
-            // for (let step = start; step < start + maxImages; step++) {
-            for (let step = start; step < febImages.length; step++) {
-                imgIndex = step % febImages.length
+            // For each image, set a thumbnail of it
+            for (let step = 0; step < febImages.length; step++) {
+                // We create a new <img> tag to hold the thumbnail and add it to the scroll
                 febiImg = document.createElement('img');
                 febiImg.className = "thumb-image"
-                // febiImg.src = 
                 imgRow.appendChild(febiImg)
-                // console.log("im width:", febiImg.clientWidth)
-                // console.log("im width:", febiImg.offsetWidth)
-                // console.log("im width:", febiImg.getBoundingClientRect().height)
+                // Width of the page so we can load appropriate image sizes
                 pageWidth = getWidth()
                 imgWidth = pageWidth*0.23
-                febiImg.src = whichImageForSize(febImages[imgIndex], imgWidth)
-                // febiImg.src = febImages[imgIndex]
+                // TODO: Do this after layout?? Saves time
+                febiImg.src = whichImageForSize(febImages[step], imgWidth)
+                console.log("Loaded: ", febiImg.src)
+                // If you click the thumbnail it moves to become the main image
                 febiImg.addEventListener('click', setImageMain)
-                // febiImg.thismainimg = mainim
+                // Info about thumbnail. Which product it belongs to and position in the scroll
                 febiImg.prodIndx = productIndex
                 febiImg.imageIndx = step
             }
-            imchildrn = imgRow.childNodes;
-            console.log("main size: ", mainImgs[0].width)
-            console.log("size: ", imchildrn.length)
-            for (let step = start; step < imchildrn.length; step++) {
-                chwidth = imchildrn[step].clientWidth
-                chwidth = imchildrn[step].width
-                console.log("Ch width:", chwidth)
-            }
-
         }
 
+        
+        window.addEventListener("load", function(){
+            imgRow = elemByIndex(0, "scroll-thumbnail")
+            imchildrn = imgRow.childNodes;
+            console.log("main size: ", mainImgs[0].width)
+            console.log("number thumbs: ", imchildrn.length)
+            for (let step = 0; step < imchildrn.length; step++) {
+                chcwidth = imchildrn[step].clientWidth
+                chwidth = imchildrn[step].width
+                console.log("Ch width:", chwidth)
+                console.log("Ch client width:", chcwidth)
+            }
+
+        });
+
+        // Get the width of the screen
         function getWidth() {
             return Math.max(
               document.body.scrollWidth,
@@ -80,78 +76,65 @@
             );
           }
 
-
+        // Set the clicked thumnnail to be the main image
         function setImageMain(evt) {
-            // Set the source of the clicked image to be the source of the main image
             console.log("Main size: ", mainImgs[evt.target.prodIndx].clientWidth)
+            // Get full size image for the thumbnail
             pathToBigSize = fullImPathFromSmall(evt.target.currentSrc)
+            // Get an appropriate size for the full image
             appropriateSize = whichImageForSize(pathToBigSize, getWidth())
+            // Replace main image with the newly decided image
             mainImgs[evt.target.prodIndx].src = appropriateSize;
+            // Update the main <img> tag with index of which image it is
             mainImgs[evt.target.prodIndx].imageIndx = evt.target.imageIndx
-            console.log(evt.target.width)
         }
 
+        // Get the appropriate full size 'main' image from its thumbnail
+        // Example:     product1/480/img.png  ->   product1/img.png
         function fullImPathFromSmall(pathToSmall){
-            
             var res = pathToSmall.split("/");
             filename = res.pop()
-            folderToDrop = res.pop()    // Don't user this var
+            // Drop the thumbnail folder
+            res.pop()    
             res.push(filename)
-            
             res = pathJoin(res, '/');
-            console.log("Old: ", pathToSmall, ", new: ", res)
             return res
         }
 
-        function whichImageForSize(path, imWidth){
-            // path example:    /path/img/1-1.jpg
-            // Wanted path:     /path/img/240/1-1.jpg
-            choosenSize = null;
-            console.log("Fetching im for size: ", imWidth)
-            for (let i=0; i<folderSizes.length; i++){
-                // console.log("wafsgasFetching im for size: ", imWidth)
-                thisSize = parseInt(folderSizes[i])
-                // console.log("Checking x,y: ", thisSize, " ", imWidth)
-                if (thisSize > imWidth){
-                
-                    // TODO: Different file systems have forward slash
-                    var res = path.split("\\");
-                    if (res.length <= 1){
-                        res = path.split("/");
-                    }
-                    filename = res.pop()
-                    // console.log('Loading file: ', res)
-                    res.push(folderSizes[i])
-                    // console.log('Loading file: ', res)
-                    res.push(filename)
-                    // console.log('Loading file: ', res)
-                    res = makeRelPath(res)
-                    // console.log('LRel path: ', res)
-                    res = pathJoin(res, '/');   // TODO: should be backslash?? 
-                    // console.log('Loading file: ', res)
-                    // if(res.exists()){
-                    //     print("Exists!")
-                    // } else {
-                    //     print("Nada on the exists")
-                    // }
-                    // console.log("Decided on ", res, ", size: ", folderSizes[i])
-                    return res
-                }
-            }
-            
-            var res = path.split("\\");
+        // Get a thumbnail of desired size from the path to a full image
+        // Example:
+        //          input:      product1/img.jpg, 480
+        //          return:     product1/480/img.jpg
+        function smallPathFromFull(imgPath, size){
+            // Different file systems have forward slash
+            var res = imgPath.split("\\");
             if (res.length <= 1){
-                res = path.split("/");
+                res = imgPath.split("/");
             }
             filename = res.pop()
-            res.push('960')
+            res.push(size)
             res.push(filename)
             res = makeRelPath(res)
             res = pathJoin(res, '/');   // TODO: should be backslash?? 
-            // if(res.exists()){
-            return res;
+            return res
         }
 
+        // Return an image of next highest width for the input width
+        // example:
+        //      input:       /path/img/1-1.jpg, 207
+        //      return:      /path/img/240/1-1.jpg
+        function whichImageForSize(path, imWidth){
+            choosenSize = null;
+            for (let i=0; i<folderSizes.length; i++){
+                thisSize = parseInt(folderSizes[i])
+                if (thisSize > imWidth){
+                    return smallPathFromFull(path, thisSize)
+                }
+            }
+            return smallPathFromFull(path, '960');
+        }
+
+        // Convert absolute path into path relative to static folder
         function makeRelPath(pathArray){
             newPathArray = []
             for (let x=pathArray.length -1; x != 0; x--){
@@ -165,14 +148,18 @@
             return newPathArray
         }
 
+        // Create a directory path from a list of strings
+        // ['path', 'to', 'folder']     ->      '/path/to/folder'
         function pathJoin(parts, sep){
             var separator = sep || '/';
             var replace   = new RegExp(separator+'{1,}', 'g');
             return parts.join(separator).replace(replace, separator);
          }
 
+        // Move either left or right by 1 image based on where the user has clicked
+        // Click on the left 1/3 to move to the left, and vice versa
         function clickPrevNext(evt){
-            thisProdImages = allims[evt.target.prodIndx]
+            thisProdImages = PRODUCT_IMAGES[evt.target.prodIndx]
             thisThumbnailIndex = evt.target.imageIndx
             imglen = evt.target.width;
             
@@ -193,12 +180,15 @@
             } 
         }
 
-
+        // Return the nth element which matches given class name
         function elemByIndex(indx, clsName){
             choiceBlocks = document.getElementsByClassName(clsName);
             return choiceBlocks[indx]
         }
 
+
+        
+        // let startIndex = [0, 0]
         // function rotateImages(evt) {
         //     imgindx = evt.target.imageIndex
         //     startIndex[imgindx] = startIndex[imgindx] + 1
